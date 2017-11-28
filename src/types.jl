966944732ecbe804ef5b987e1357647a4e5bc51f
roger <roger.herikstad@gmail.com>
2017-11-28 10:06:12 +0800

    Adds type for Ripple basic header

---
+++ src/types.jl
@@ -0,0 +1,13 @@
+struct BasicHeader
+    filetype_id::SVector{8, UInt8}
+    filespec::SVector{2, UInt8}
+    nbytes::UInt32
+    label::SVector{16, UInt8}
+    comments::SVector{200, UInt8}
+    createapp::SVector{52, UInt8}
+    processor_timestamp::UInt32
+    period::UInt32
+    time_resolution::UInt32
+    time_origin::SVector{8, UInt16}
+    nchannels::UInt32
+end
