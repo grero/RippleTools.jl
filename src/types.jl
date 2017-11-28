7a6e9b7812fd5bb041ef571b6ab2d42b34e2d341
roger <roger.herikstad@gmail.com>
2017-11-28 16:18:29 +0800

    Adds method to load header from file handle

--- src/types.jl
+++ src/types.jl
@@ -11,3 +11,10 @@ struct BasicHeader
     time_origin::SVector{8, UInt16}
     nchannels::UInt32
 end
+
+function BasicHeader(ff::IOStream)
+    bytes = read(ff, sizeof(BasicHeader))
+    unsafe_load(convert(Ptr{BasicHeader}, pointer(bytes)))
+end
+
+get_filtype(hh::BasicHeader) = unsafe_string(pointer(convert(Array{UInt8, 1}, hh.filetype_id)),length(hh.filetype_id))
