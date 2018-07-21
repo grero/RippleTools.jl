b4c236b8596da751013e23511e00f8ab528feb1c
roger <roger.herikstad@gmail.com>
2018-07-21 11:19:32 +0800

    Removes dependency on PyCall and PyNS

--- src/RippleTools.jl
+++ src/RippleTools.jl
@@ -1,9 +1,5 @@
 module RippleTools
-using PyCall
 using StaticArrays
-@pyimport pyns
-const NSFile = pyns.nsfile[:NSFile]
-const EntityType = pyns.nsentity[:EntityType]
 using FileIO
 
 try
@@ -24,23 +20,6 @@ function load(ff::File{format"NSX"})
 end
 
 bit_order = [4, 5, 7, 1, 10, 12, 13, 15]
-function get_rawdata(fname,channel="all")
-    f = NSFile(fname)
-    analog_entities = f[:get_entities](EntityType[:analog])
-    data = Dict{Int64,Array{Float64,1}}()
-    for entity in analog_entities
-        _info = entity[:get_analog_info]()
-        _einfo = entity[:get_entity_info]()
-        tt, _ch = split(_einfo[1])
-        ch = parse(Int64,_ch)
-        if channel == "all" || ((ch in channel) && (tt == "raw"))
-            if _info[1] > 1000  # sampling rate
-                data[ch] = entity[:get_analog_data]()
-            end
-        end
-    end
-    data
-end
 
 function extract_markers(fname)
     header = open(fname,"r") do ff
