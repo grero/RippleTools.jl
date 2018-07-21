c8773371bea707650b74780219ddfe66e8206e5a
roger <roger.herikstad@gmail.com>
2018-07-21 10:44:17 +0800

    Fixes and extends tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,7 +24,28 @@ using Base.Test
             run(`ls`)
             run(`unzip Ripple_Sample_Data_Set_v1.zip`)
         end
-        pp = load("sample_data_set.nev")
+        @testset "Test header" begin
+            header = open("sample_data_set.nev") do ff
+                read(ff, RippleTools.BasicNEVHeader)
+            end
+            appname = unsafe_string(pointer(Vector(header.createap)))
+            @test appname == "Trellis[1.9.0.313]"
+            comments = unsafe_string(pointer(Vector(header.comments)))
+            @test comments == ""
+            @test Vector(header.time_origin) == [2018,3,5,2,21,53,41,625]
+            @test header.processor_timestamp == 34198620
+            @test header.n_extended_headers == 165
+        end
+        @testset "Test packet" begin
+            pp = load("sample_data_set.nev")
+            @test typeof(pp[1][1]) <: RippleTools.EventDataPacket{94}
+            qq = filter(p->p.reason==0x04, pp[1])
+            @test length(qq) == 218
+            qv = filter(p->p.sma[2] == 32767, qq)
+            @test length(qv) == 109
+
+            @test typeof(pp[2][1].waveform) <: RippleTools.SVector{52,Int16}
+        end
     end
 end
 
