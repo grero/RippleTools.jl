38ba008bf5b56f2e44c50b50b4006cee43bfe50d
roger <roger.herikstad@gmail.com>
2018-11-26 15:09:19 +0800

    Fixes some deprecation warnings

--- src/events.jl
+++ src/events.jl
@@ -3,10 +3,10 @@ function load(ff::File{format"NEV"})
         fio = f.io
         header = BasicNEVHeader(fio)  
         #grap the extended headers
-        wave_headers = Vector{WaveEventHeader}(0)
-        filter_headers = Vector{FilterEventHeader}(0)
-        label_headers = Vector{LabelEventHeader}(0)
-        dig_label_headers = Vector{DigitalLabelEventHeader}(0)
+        wave_headers = WaveEventHeader[]
+        filter_headers = FilterEventHeader[]
+        label_headers = LabelEventHeader[]
+        dig_label_headers = DigitalLabelEventHeader[]
         wf_type = Int16
         for i in 1:header.n_extended_headers
             eh = get_eheader(fio)
@@ -26,9 +26,9 @@ function load(ff::File{format"NEV"})
         end
         Ne = header.nbytes_packets-18
         N = div(header.nbytes_packets-8, sizeof(wf_type))
-        event_packets = Vector{EventDataPacket{Ne}}(0)
-        spike_packets = Vector{SpikeDataPacket{wf_type,N}}(0)
-        stim_packets = Vector{StimDataPacket{wf_type,N}}(0)
+        event_packets = EventDataPacket{Ne}[]
+        spike_packets = SpikeDataPacket{wf_type,N}[]
+        stim_packets = StimDataPacket{wf_type,N}[]
         seek(fio, header.nbytes)
         while !eof(fio)
             packet = get_packet!(fio, header, wf_type)
