4a7b22018b962949f4d86d55ea9e14b6c14a9655
Roger Herikstad <roger.herikstad@gmail.com>
2019-12-12 15:41:10 +0800

    Removes debug output

--- src/types.jl
+++ src/types.jl
@@ -155,7 +155,6 @@ Seek the stream in units of a full data point, i.e. by number of channels
 """
 function Base.seek(reader::DataPacketStreamer, pos)
 	if 0 <= pos < reader.npoints
-		println("Seeking...")
 		seek(reader.io, reader.offset + pos*reader.nchannels*2)
 		reader.position = pos
 	end
